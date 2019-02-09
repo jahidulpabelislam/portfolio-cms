cd47e9bb01c3d706b351e3f5019b1afda5b551bf
Jahidul Pabel Islam <jahidul.pabel.islam@hotmail.com>
2019-02-09 22:18:28 +0000

    better checkbox styling

--- partials/projects-select.php
+++ partials/projects-select.php
@@ -8,8 +8,8 @@ if (!defined("ROOT")) {
 
                     <div class="projects-select__form">
                         <div ng-repeat="project in projects" class="projects-select__option styled-checkbox">
-                            <label ng-click="selectProject(project)" for="{{ project.id }}">
-                                <input type="radio" id="{{ project.id }}" name="project" value="{{ project.id }}" class="input">
+                            <label ng-click="selectProject(project)" for="{{ project.id }}" class="styled-checkbox__label">
+                                <input type="radio" id="{{ project.id }}" name="project" value="{{ project.id }}" class="input checkbox">
                                 {{project.name}}<span class="styled-checkbox__pseudo js-styled-checkout"></span>
                             </label>
                         </div>

7a4c970541eb6d9216b86928f8cbbd1188f6f5b9
Jahidul Pabel Islam <me@jahidulpabelislam.com>
2019-04-17 23:40:46 +0100

    Update project inputs ordering

--- partials/project-view.php
+++ partials/project-view.php
@@ -15,45 +15,22 @@ if (!defined("ROOT")) {
                     </a>
 
                     <form id="projectForm" class="project__form" ng-submit="checkAuthStatus(submitProject)">
-                        <label for="projectName">Project Name <span class="required">*</span></label>
+                        <label for="projectName">Project Name: <span class="required">*</span></label>
                         <input ng-model="selectedProject.name" type="text" name="projectName" id="projectName" class="input" placeholder="myproject" tabindex="7" oninput="jpi.helpers.checkInputField(this);" required />
 
-                        <label for="skill-input">Skills <span class="required">*</span></label>
-
-                        <div ng-model="selectedProject.skills" ui-sortable class="ui-state-default">
-                            <p ng-repeat="skill in selectedProject.skills" class="project__skill project__skill--{{ selectedProject.colour }}">{{ skill }}
-                                <button class="btn project__skill-delete-button" ng-click="deleteSkill(skill)" type="button">x</button>
-                            </p>
-                        </div>
-
-                        <div class="project__skill-input-container">
-                            <label for="skill-input" class="screen-reader-text">Add skills for project.</label>
-                            <input type="text" class="input project__skill-input" id="skill-input" placeholder="HTML5" ng-model="skillInput" />
-                            <button class="btn btn--green project__skill-add-button" type="button" id="skill-add" ng-click="addSkill()" type="button">
-                                <span class="screen-reader-text">Add</span>
-                                <i class="fa fa-plus"></i>
-                            </button>
-                        </div>
-
-                        <label for="longDescription">Long Description <span class="required">*</span></label>
-                        <textarea ng-model="selectedProject.long_description" name="description" id="longDescription" class="input" placeholder="description" tabindex="9" oninput="jpi.helpers.checkInputField(this);" required rows="10"></textarea>
-
-                        <label for="shortDescription">Short Description <span class="required">*</span></label>
-                        <textarea ng-model="selectedProject.short_description" name="description" id="shortDescription" class="input" placeholder="description" tabindex="9" oninput="jpi.helpers.checkInputField(this);" required rows="10"></textarea>
-
-                        <label for="link">Link</label>
-                        <input ng-model="selectedProject.link" type="text" name="link" id="link" class="input" placeholder="link" tabindex="10" />
-
-                        <label for="github">GitHub <span class="required">*</span></label>
-                        <input ng-model="selectedProject.github" type="url" name="github" id="github" class="input" placeholder="github" tabindex="11" oninput="jpi.helpers.checkInputField(this);" required />
-
-                        <label for="download">Download</label>
-                        <input ng-model="selectedProject.download" type="text" name="download" id="download" class="input" placeholder="download" tabindex="12" />
-
-                        <label for="date">Date <span class="required">*</span></label>
+                        <label for="date">Date: <span class="required">*</span></label>
                         <input ng-model="selectedProject.date" type="date" name="date" id="date" class="input" placeholder="2016-01-30" tabindex="13" oninput="jpi.helpers.checkInputField(this);" required />
 
-                        <label for="colour">Colour </label>
+                        <label for="link">Link:</label>
+                        <input ng-model="selectedProject.link" type="text" name="link" id="link" class="input" placeholder="link" tabindex="10" />
+
+                        <label for="github">GitHub: <span class="required">*</span></label>
+                        <input ng-model="selectedProject.github" type="url" name="github" id="github" class="input" placeholder="github" tabindex="11" oninput="jpi.helpers.checkInputField(this);" required />
+
+                        <label for="download">Download:</label>
+                        <input ng-model="selectedProject.download" type="text" name="download" id="download" class="input" placeholder="download" tabindex="12" />
+
+                        <label for="colour">Colour:</label>
                         <select ng-model="selectedProject.colour" name="colour" id="colour" class="input" tabindex="14">
                             <?php
                             $colourOptions = [
@@ -70,6 +47,29 @@ if (!defined("ROOT")) {
                             }
                             ?>
                         </select>
+
+                        <label for="skill-input">Skills: <span class="required">*</span></label>
+
+                        <div ng-model="selectedProject.skills" ui-sortable class="ui-state-default">
+                            <p ng-repeat="skill in selectedProject.skills" class="project__skill project__skill--{{ selectedProject.colour }}">{{ skill }}
+                                <button class="btn project__skill-delete-button" ng-click="deleteSkill(skill)" type="button">x</button>
+                            </p>
+                        </div>
+
+                        <div class="project__skill-input-container">
+                            <label for="skill-input" class="screen-reader-text">Add skills for project.</label>
+                            <input type="text" class="input project__skill-input" id="skill-input" placeholder="HTML5" ng-model="skillInput" />
+                            <button class="btn btn--green project__skill-add-button" type="button" id="skill-add" ng-click="addSkill()" type="button">
+                                <span class="screen-reader-text">Add</span>
+                                <i class="fa fa-plus"></i>
+                            </button>
+                        </div>
+
+                        <label for="shortDescription">Short Description: <span class="required">*</span></label>
+                        <textarea ng-model="selectedProject.short_description" name="description" id="shortDescription" class="input" placeholder="description" tabindex="9" oninput="jpi.helpers.checkInputField(this);" required rows="10"></textarea>
+
+                        <label for="longDescription">Long Description: <span class="required">*</span></label>
+                        <textarea ng-model="selectedProject.long_description" name="description" id="longDescription" class="input" placeholder="description" tabindex="9" oninput="jpi.helpers.checkInputField(this);" required rows="10"></textarea>
 
                         <!-- Div containing all the project images -->
                         <ul ui-sortable ng-model="selectedProject.images" class="project__images-container ui-state-default">

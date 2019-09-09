b5780a974bfa5e6f7cec6ee87a6447574d65f4dd
Jahidul Pabel Islam <jahidul@jahidulpabelislam.com>
2019-09-09 22:25:11 +0100

    Display table of projects instead of list

--- partials/projects-select.php
+++ partials/projects-select.php
@@ -6,36 +6,52 @@ if (!defined("ROOT")) {
 
                 <div class="projects-select" ng-show="isLoggedIn">
 
-                    <div class="projects-select__form">
-                        <div ng-repeat="project in projects" class="projects-select__option styled-checkbox">
-                            <label for="project-{{ project.id }}" class="styled-checkbox__label">
-                                <input type="radio" id="project-{{ project.id }}" name="project" value="{{ project.id }}" class="input checkbox" ng-model="selectedProject" ng-change="selectProject(project)" tabindex="1" />
-                                {{ project.name }}<span class="styled-checkbox__pseudo js-styled-checkout"></span>
-                            </label>
-                        </div>
-                    </div>
-
                     <p class="feedback feedback--error projects-select__feedback" ng-show="selectProjectFeedback">
                         {{ selectProjectFeedback }}
                     </p>
 
                     <div>
-                        <a class="btn btn--dark-blue projects-select__edit-button js-edit-project" href="/project/{{ selectedProject.id }}/edit/" title="Link to Edit Project Form Page" ng-show="projects.length > 0" ng-disabled="!selectedProject.id" tabindex="1">
-                            <span class="screen-reader-text">Edit</span>
-                            <i class="fas fa-edit"></i>
-                        </a>
-                        <button type="button" class="btn btn--red projects-select__delete-button" ng-show="projects.length > 0" ng-click="checkAuthStatus(deleteProject)" tabindex="1">
-                            <span class="screen-reader-text">Delete</span>
-                            <i class="fas fa-trash"></i>
-                        </button>
                         <a class="btn btn--dark-green projects-select__add-button js-new-project" href="/project/new/" title="Link to New Project Form Page" tabindex="1">
-                            <span class="screen-reader-text">Add A Project</span>
+                            <span class="screen-reader-text">Add A New Project</span>
                             <i class="fas fa-plus"></i>
                         </a>
                     </div>
 
+                    <table class="projects-select__table table">
+                        <thead>
+                            <tr class="table__row">
+                                <th>ID</th>
+                                <th>Name</th>
+                                <th>Date</th>
+                                <th>Last Modified</th>
+                                <th>Created at</th>
+                                <th>-</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr class="table__row" ng-repeat="project in projects">
+                                <td>{{ project.id }}</td>
+                                <td>{{ project.name }}</td>
+                                <td>{{ project.date | date: 'EEEE d MMMM yyyy' }}</td>
+                                <td>{{ project.updated_at | date: 'EEEE d MMMM yyyy' }}</td>
+                                <td>{{ project.created_at | date: 'EEEE d MMMM yyyy' }}</td>
+                                <td>
+                                    <a class="btn btn--dark-blue projects-select__edit-button js-edit-project" href="/project/{{ project.id }}/edit/" title="Link to Edit Project Form Page" tabindex="1" ng-click="selectProject(project)">
+                                        <span class="screen-reader-text">Edit</span>
+                                        <i class="fas fa-edit"></i>
+                                    </a>
+
+                                    <button type="button" class="btn btn--red projects-select__delete-button" ng-click="selectProject(project); checkAuthStatus(deleteProject);" tabindex="1">
+                                        <span class="screen-reader-text">Delete</span>
+                                        <i class="fas fa-trash"></i>
+                                    </button>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+
                     <ul class="pagination" ng-show="pages.length > 1">
-                        <li class="pagination__item"ng-repeat="page in pages" >
+                        <li class="pagination__item" ng-repeat="page in pages" >
                             <a class="pagination__item-link js-projects" href="/projects/{{ page }}/" title="Link to Projects Page" ng-class="{'active': page == currentPage}" data-page="{{ page }}" tabindex="1">{{ page }}</a>
                         </li>
                     </ul>

cb4e7b62f4bc33f5bfb7f39a9eda239774fe9ecf
Jahidul Pabel Islam <jahidul@jahidulpabelislam.com>
2020-01-26 13:29:54 +0000

    Tidy up gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,57 +1,55 @@
-const gulp = require("gulp"),
+const gulp = require("gulp");
 
-    concat = require("gulp-concat"),
+const concat = require("gulp-concat");
 
-    uglify = require("gulp-uglify"),
+const uglify = require("gulp-uglify");
 
-    cleanCss = require("gulp-clean-css"),
-    autoPrefix = require("gulp-autoprefixer"),
+const cleanCss = require("gulp-clean-css");
+const autoPrefix = require("gulp-autoprefixer");
 
-    sass = require("gulp-sass");
+const sass = require("gulp-sass");
 
 const assetsDir = "./assets";
 const cssDir = `${assetsDir}/css`;
 const jsDir = `${assetsDir}/js`;
 
-let defaultTasks = [];
+const defaultTasks = [];
 
 // Concatenate & Minify JS
-const scripts = {
-    main: [
-        `${jsDir}/third-party/jquery.min.js`,
-        `${jsDir}/third-party/jquery-ui.min.js`,
-        `${jsDir}/third-party/angular.min.js`,
-        `${jsDir}/third-party/tinymce/tinymce.min.js`,
-        `${jsDir}/third-party/angular-ui-tinymce.min.js`,
-        `${jsDir}/third-party/sortable.js`,
-        `${jsDir}/third-party/sticky-footer.min.js`,
-        `${jsDir}/jpi/helpers.js`,
-        `${jsDir}/jpi/drag-n-drop.js`,
-        `${jsDir}/jpi/nav.js`,
-    ],
-};
-const scriptNames = Object.keys(scripts);
-
-let scriptTasks = [];
-scriptNames.forEach(function(key) {
-    const scriptTask = `scripts-${key}`;
-    scriptTasks.push(scriptTask);
-    gulp.task(scriptTask, function() {
-        return gulp.src(scripts[key])
-                   .pipe(concat(`${key}.min.js`))
-                   .pipe(uglify())
-                   .pipe(gulp.dest(`${jsDir}/`));
-    });
-});
-gulp.task("scripts", gulp.parallel(scriptTasks));
 defaultTasks.push("scripts");
+gulp.task("scripts", function(callback) {
+    const scripts = {
+        main: [
+            `${jsDir}/third-party/jquery.min.js`,
+            `${jsDir}/third-party/jquery-ui.min.js`,
+            `${jsDir}/third-party/angular.min.js`,
+            `${jsDir}/third-party/tinymce/tinymce.min.js`,
+            `${jsDir}/third-party/angular-ui-tinymce.min.js`,
+            `${jsDir}/third-party/sortable.js`,
+            `${jsDir}/third-party/sticky-footer.min.js`,
+            `${jsDir}/jpi/helpers.js`,
+            `${jsDir}/jpi/drag-n-drop.js`,
+            `${jsDir}/jpi/nav.js`,
+        ],
+    };
+    const scriptNames = Object.keys(scripts);
 
+    scriptNames.forEach(function(key) {
+        gulp.src(scripts[key])
+            .pipe(concat(`${key}.min.js`))
+            .pipe(uglify())
+            .pipe(gulp.dest(`${jsDir}/`));
+    });
+
+    callback();
+});
+
+defaultTasks.push("sass");
 gulp.task("sass", function() {
     return gulp.src(`${cssDir}/jpi/main.scss`)
                .pipe(sass().on("error", sass.logError))
                .pipe(gulp.dest(`${cssDir}/jpi/`));
 });
-defaultTasks.push("sass");
 
 // Watch SASS files For changes to compile to css
 gulp.task("watch", function() {
@@ -59,31 +57,29 @@ gulp.task("watch", function() {
 });
 
 // Minify Stylesheets
-const stylesheets = {
-    main: [
-        `${cssDir}/jpi/main.css`,
-    ],
-};
-const stylesheetNames = Object.keys(stylesheets);
-
-let stylesheetTasks = [];
-stylesheetNames.forEach(function(key) {
-    const stylesheetTask = `styles-${key}`;
-    stylesheetTasks.push(stylesheetTask);
-    gulp.task(stylesheetTask, function() {
-        return gulp.src(stylesheets[key])
-                   .pipe(concat(`${key}.min.css`))
-                   .pipe(autoPrefix({
-                       browsers: ["> 0.1%", "ie 8-11"],
-                       remove: false,
-                   }))
-                   .pipe(cleanCss({
-                       compatibility: "ie8",
-                   }))
-                   .pipe(gulp.dest(`${cssDir}/jpi/`));
-    });
-});
-gulp.task("styles", gulp.parallel(stylesheetTasks));
 defaultTasks.push("styles");
+gulp.task("styles", function(callback) {
+    const stylesheets = {
+        main: [
+            `${cssDir}/jpi/main.css`,
+        ],
+    };
+    const stylesheetNames = Object.keys(stylesheets);
+
+    stylesheetNames.forEach(function(key) {
+        gulp.src(stylesheets[key])
+            .pipe(concat(`${key}.min.css`))
+            .pipe(autoPrefix({
+                browsers: ["> 0.1%", "ie 8-11"],
+                remove: false,
+            }))
+            .pipe(cleanCss({
+                compatibility: "ie8",
+            }))
+            .pipe(gulp.dest(`${cssDir}/jpi/`));
+    });
+
+    callback();
+});
 
 gulp.task("default", gulp.series(defaultTasks));

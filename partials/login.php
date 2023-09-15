92642882542a3c66f5e3b9fc5e4a23581c1c126e
Jahidul Pabel Islam <jahidul@jahidulpabelislam.com>
2023-09-15 21:46:43 +0100

    Log In form styling tweaks

--- partials/login.php
+++ partials/login.php
@@ -2,15 +2,15 @@
     <div class="container">
         <form class="login__form">
             <div class="input-group">
-                <label for="username">Username</label>
-                <input type="text" class="input" id="username" placeholder="myusername" tabindex="1" oninput="jpi.helpers.checkInput(this);" autofocus />
+                <label for="username" class="visually-hidden">Username</label>
+                <input type="text" class="input" id="username" placeholder="Username" tabindex="1" oninput="jpi.helpers.checkInput(this);" autofocus />
             </div>
             <div class="input-group">
-                <label for="password">Password</label>
-                <input type="password" class="input" id="password" placeholder="mypassword" tabindex="1" oninput="jpi.helpers.checkInput(this);" />
+                <label for="password" class="visually-hidden">Password</label>
+                <input type="password" class="input" id="password" placeholder="Password" tabindex="1" oninput="jpi.helpers.checkInput(this);" />
             </div>
             <p class="feedback feedback--error login__feedback"></p>
-            <button type="submit" class="btn btn--dark-green login__submit">Log in</button>
+            <button type="submit" class="btn btn--dark-green login__submit">Log In</button>
         </form>
     </div>
 </div>

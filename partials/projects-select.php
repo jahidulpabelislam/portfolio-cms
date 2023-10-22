65aa1d635630a0e006a721be4fa543705973fd3b
Jahidul Pabel Islam <jahidul@jahidulpabelislam.com>
2023-10-22 17:23:45 +0100

    Projects listing styling updates

--- partials/projects-select.php
+++ partials/projects-select.php
@@ -26,7 +26,7 @@ $projectTypes = $apiRes["data"];
 
     <form class="projects-select__filters">
         <div class="projects-select-filter projects-select-filter--search projects-select-filter-search">
-            <input class="input js-filters-search" type="text" value="" placeholder="Search..." />
+            <input class="input js-filters-search" type="text" value="" placeholder="Search ..." />
         </div>
         <div class="projects-select-filter">
             <select class="input js-filters-type js-filters-on-change">
@@ -37,6 +37,9 @@ $projectTypes = $apiRes["data"];
             </select>
         </div>
         <div class="projects-select-filter">
+            <input class="input js-filters-date js-filters-on-change" type="date" value="" />
+        </div>
+        <div class="projects-select-filter">
             <div class="projects-select__option styled-checkbox">
                 <label for="projects-select-filter-published" class="styled-checkbox__label">
                     <input type="checkbox" class="input checkbox js-filters-published js-filters-on-change" id="projects-select-filter-published" value="true" />
@@ -44,19 +47,17 @@ $projectTypes = $apiRes["data"];
                 </label>
             </div>
         </div>
-        <div class="projects-select-filter">
-            <input class="input js-filters-date js-filters-on-change" type="date" value="" />
-        </div>
     </form>
 
     <table class="projects-select__table table table--sticky">
         <thead>
             <tr class="table__row">
                 <th class="table__header">Name</th>
+                <th class="table__header">Type</th>
                 <th class="table__header">Date</th>
-                <th class="table__header">Created At</th>
                 <th class="table__header">Updated At</th>
-                <th class="table__header table__header--center">-</th>
+                <th class="table__header">Is Published?</th>
+                <th class="table__header"></th>
             </tr>
         </thead>
         <tbody></tbody>
@@ -68,13 +69,14 @@ $projectTypes = $apiRes["data"];
 <script type="text/template" id="js-projects-table-row">
     <tr class="table__row">
         <td class="table__column" data-title="Name">{{ name }}</td>
+        <td class="table__column" data-title="Type">{{ type }}</td>
         <td class="table__column" data-title="Date">{{ date }}</td>
-        <td class="table__column" data-title="Created At">{{ created_at }}</td>
         <td class="table__column" data-title="Last Modified">{{ updated_at }}</td>
-        <td class="table__column table__column--center table__column--no-padding">
+        <td class="table__column" data-title="Is Published?">{{ status }}</td>
+        <td class="table__column table__column--right">
             <a class="projects-select__edit-button" href="/project/edit/{{ id }}/" data-id="{{ id }}" title="Link to Edit Project Form Page" tabindex="1">
                 <span class="visually-hidden">Edit</span>
-                <span class="material-symbols-outlined">edit</span>
+                <span class="material-symbols-outlined">edit_square</span>
             </a>
 
             <button type="button" class="projects-select__delete-button" data-id="{{ id }}" tabindex="1">

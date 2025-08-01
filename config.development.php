926d453aeb3a1300f8aa78183c01dd986a08e492
Jahidul Pabel Islam <jahidul@d3r.com>
2025-08-01 23:07:10 +0100

    Update dev api domain

--- config.development.php
+++ config.development.php
@@ -1,5 +1,5 @@
 <?php
 
 if (!defined("JPI_API_ENDPOINT")) {
-    define("JPI_API_ENDPOINT", "http://portfolio-api.local/");
+    define("JPI_API_ENDPOINT", "http://api.jahidulpabelislam.local");
 }
